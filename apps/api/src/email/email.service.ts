d0b62e292592a0107c27632eb8b3917c90def485
segawa <>
2026-02-22 20:01:21 +0900

    fix: renderでメール送信周りがエラーになる

--- apps/api/src/email/email.service.ts
+++ apps/api/src/email/email.service.ts
@@ -9,7 +9,10 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      family: 4,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
