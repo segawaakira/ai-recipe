66c020a4869429e2faada4875ef59385bc892097
segawa <>
2026-08-08 01:08:20 +0900

    chore: BASIC認証無効

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,22 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const basicAuth = Buffer.from(
-  `${process.env.AUTH_USER}:${process.env.AUTH_PASS}`
-).toString("base64");
+// BASIC認証を無効化
+// const basicAuth = Buffer.from(
+//   `${process.env.AUTH_USER}:${process.env.AUTH_PASS}`
+// ).toString("base64");
 
-export function middleware(req: NextRequest) {
-  const authHeader = req.headers.get("authorization");
+export function middleware(_req: NextRequest) {
+  // const authHeader = _req.headers.get("authorization");
 
-  if (authHeader === `Basic ${basicAuth}`) {
-    return NextResponse.next();
-  }
+  // if (authHeader === `Basic ${basicAuth}`) {
+  //   return NextResponse.next();
+  // }
 
-  return new NextResponse("Authentication required", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Secure Area"',
-    },
-  });
+  // return new NextResponse("Authentication required", {
+  //   status: 401,
+  //   headers: {
+  //     "WWW-Authenticate": 'Basic realm="Secure Area"',
+  //   },
+  // });
+
+  return NextResponse.next();
 }
 
 export const config = {
